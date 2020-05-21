1426e83f1f188d6b5cc8042dc09a63601f16eee7
fuyaoyao <53067825+fuyaoyao77@users.noreply.github.com>
2020-05-21 10:50:54 +0800

    Create Array27.cpp

---
+++ Array27.cpp
@@ -0,0 +1,75 @@
+/*
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+
+示例 1:
+
+给定 nums = [3,2,2,3], val = 3,
+
+函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+示例 2:
+
+给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+
+函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+
+注意这五个元素可为任意顺序。
+
+你不需要考虑数组中超出新长度后面的元素。
+
+*/
+
+//解法一
+
+//双指针
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i, j = 0;
+        for(i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+        
+    }
+};
+
+//解法2
+
+//合理利用hash函数+双指针
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        std::map<int,int> map;
+        int i, j = 0;
+        for(i = 0; i < nums.size(); i++){
+            map[nums[i]] = 0;
+        }
+
+        for(i = 0; i < nums.size(); i++){
+            if(nums[i] == val){
+                ++map[nums[i]];
+            }
+        }
+
+        for(i = 0; i < nums.size(); i++){
+            if(map[nums[i]] == 0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+        
+    }
+};
