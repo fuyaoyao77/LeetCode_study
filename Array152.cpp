7e2eecc8b0579e5712fe42d2de04f0032041a9d4
fuyaoyao <53067825+fuyaoyao77@users.noreply.github.com>
2020-05-30 18:28:43 +0800

    Create Array152.cpp

---
+++ Array152.cpp
@@ -0,0 +1,43 @@
+
+//使用动态规划，找出相应的动态规划转移方程
+/*
+给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+ 
+
+示例 1:
+
+输入: [2,3,-2,4]
+输出: 6
+解释: 子数组 [2,3] 有最大乘积 6。
+示例 2:
+
+输入: [-2,0,-1]
+输出: 0
+解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+
+*/
+
+
+
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        vector <int> maxF(nums), minF(nums);
+        for (int i = 1; i < nums.size(); ++i) {
+            maxF[i] = max(maxF[i - 1] * nums[i], max(nums[i], minF[i - 1] * nums[i]));
+            minF[i] = min(minF[i - 1] * nums[i], min(nums[i], maxF[i - 1] * nums[i]));
+        }
+        return *max_element(maxF.begin(), maxF.end());
+    }
+};
+
+
+
+
+
+
+
+
