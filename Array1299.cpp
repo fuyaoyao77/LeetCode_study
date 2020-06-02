e1b8c31eec0349dbb787d4bd016a000db6797719
fuyaoyao <53067825+fuyaoyao77@users.noreply.github.com>
2020-06-02 09:17:27 +0800

    Update Array1299.cpp

--- Array1299.cpp
+++ Array1299.cpp
@@ -35,7 +35,26 @@ public:
     }
 };
 
+/*
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        ans[n - 1] = -1;
+        for (int i = n - 2; i >= 0; --i) {
+            ans[i] = max(ans[i + 1], arr[i + 1]);
+        }
+        return ans;
+    }
+};
 
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/replace-elements-with-greatest-element-on-right-side/solution/jiang-mei-ge-yuan-su-ti-huan-wei-you-ce-zui-da-y-5/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+*/
 
 
 
