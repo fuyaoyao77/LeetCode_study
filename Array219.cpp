be360b59a130d74c3149575d8ebcb3f9143d0496
fuyaoyao <53067825+fuyaoyao77@users.noreply.github.com>
2020-06-20 17:17:19 +0800

    存在重复元素 II

---
+++ Array219.cpp
@@ -0,0 +1,44 @@
+//219. 存在重复元素 II
+
+/*
+给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
+
+ 
+
+示例 1:
+
+输入: nums = [1,2,3,1], k = 3
+输出: true
+示例 2:
+
+输入: nums = [1,0,1,1], k = 1
+输出: true
+示例 3:
+
+输入: nums = [1,2,3,1,2,3], k = 2
+输出: false
+*/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       unordered_set<int> set; //搜索、插入和移除平均常数时间复杂度，不会超时
+       for(int i = 0; i < nums.size(); i++)
+       {
+            if(set.find(nums[i]) != set.end())
+                return true;
+            set.insert(nums[i]);
+            if(set.size() > k )
+               set.erase(nums[i-k]); //删除前面的元素，保持滑动窗口长度为k 
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+
+
