2af81f5bc9545cd147a3b083da4640d779a8d4ee
fuyaoyao <53067825+fuyaoyao77@users.noreply.github.com>
2020-06-29 16:31:51 +0800

    面试题 04.04. 检查平衡性

---
+++ faceTree0404.cpp
@@ -0,0 +1,62 @@
+//面试题 04.04. 检查平衡性
+
+
+/*
+实现一个函数，检查二叉树是否平衡。在这个问题中，平衡树的定义如下：任意一个节点，其两棵子树的高度差不超过 1。
+
+
+示例 1:
+给定二叉树 [3,9,20,null,null,15,7]
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+示例 2:
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+      1
+     / \
+    2   2
+   / \
+  3   3
+ / \
+4   4
+返回 false 。
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    bool ans = true;
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        if(abs(left - right) > 1) 
+            ans = false;
+        return max(left, right) + 1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        maxDepth(root);
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
