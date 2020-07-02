1d90085b9b97396b4480e4c1742f32a320e86bc2
fuyaoyao <53067825+fuyaoyao77@users.noreply.github.com>
2020-07-02 13:35:16 +0800

    有序矩阵中第K小的元素

---
+++ erfen378.cpp
@@ -0,0 +1,84 @@
+//378. 有序矩阵中第K小的元素
+
+/*
+给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第 k 小的元素。
+请注意，它是排序后的第 k 小元素，而不是第 k 个不同的元素。
+
+ 
+
+示例：
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+返回 13。
+ 
+
+提示：
+你可以假设 k 的值永远是有效的，1 ≤ k ≤ n2 。
+
+//二分法，时间复杂度大大减少
+
+class Solution {
+   public:
+    bool check(vector<vector<int>>& matrix, int mid, int k, int n) {
+        int i = n - 1;
+        int j = 0;
+        int num = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= mid) {
+                num += i + 1;
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return num >= k;
+    }
+
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/solution/you-xu-ju-zhen-zhong-di-kxiao-de-yuan-su-by-leetco/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+*/
+
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        vector<int> reshape;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                reshape.push_back(matrix[i][j]);
+            }
+        }
+        sort(reshape.begin(),reshape.end());
+        return reshape[k-1];
+    }
+};
+
+
